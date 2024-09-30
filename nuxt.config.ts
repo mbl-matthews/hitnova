b50c1c6731db48ca7491eb978771397fe3916779
mbl <mbl.85@web.de>
2024-09-30 12:11:08 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  //...
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  modules: [
+    '@nuxtjs/ionic',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
+  compatibilityDate: '2024-09-29',
+})
